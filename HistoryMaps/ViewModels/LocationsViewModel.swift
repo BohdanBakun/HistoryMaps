ee32da0fc19dc5c158ee643260922a8fff5f1f4d
Bohdan Bakun <hasps.bowler-0d@icloud.com>
2024-02-29 22:59:11 +0200

    ViewModel added

---
+++ HistoryMaps/ViewModels/LocationsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LocationsViewModel.swift
+//  HistoryMaps
+//
+//  Created by Богдан Бакун on 29.02.2024.
+//
+
+import Foundation
---
+++ HistoryMaps/Views/LocationsView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationsView.swift
+//  HistoryMaps
+//
+//  Created by Богдан Бакун on 29.02.2024.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationsView()
+}
