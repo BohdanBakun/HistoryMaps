885a5b3372d260fbac6f15ae155b577bbe5ea10f
Bohdan Bakun <hasps.bowler-0d@icloud.com>
2024-02-29 22:20:18 +0200

    Initial Commit

---
+++ HistoryMaps/HistoryMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryMapsApp.swift
+//  HistoryMaps
+//
+//  Created by Богдан Бакун on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct HistoryMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
